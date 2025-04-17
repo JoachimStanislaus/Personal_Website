29810892edd0f0e39ab346616e58dd3c687ba1dd
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-04-17 22:59:13 +0100

    some file structure and name changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
-import * as React from 'react'
-import Nav from './nav'
-import Header from './Hero'
+// import * as React from 'react'
+import Nav from './components/NavBar'
+import Header from './components/Hero'
+import About from './About'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
   const color = "teal"
@@ -13,6 +15,7 @@ function App() {
     <ChakraProvider>
       <Nav></Nav>
       <Header color={color}></Header>
+      <About color={color} count='01' subheader='About' content='test' ></About>
     </ChakraProvider>
   )
 }
