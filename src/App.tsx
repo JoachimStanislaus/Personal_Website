e1c8525780e4b30790b48b69b7e497c19e297398
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-04-17 22:09:42 +0100

    added hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import './App.css'
 import * as React from 'react'
 import Nav from './nav'
+import Header from './Hero'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
+  const color = "teal"
 
   return (
     <ChakraProvider>
       <Nav></Nav>
-      
+      <Header color={color}></Header>
     </ChakraProvider>
   )
 }
