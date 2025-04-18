9cecff16ab885080965f0009aa62040a8e4175d2
Joachim Tan <joachim@Joachims-Mac-mini.local>
2025-04-18 23:25:54 +0100

    fixing issues with npm run build for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { ChakraProvider, Text } from '@chakra-ui/react'
 import { ContentItemGenerationProps } from './types/content'
 import Contact from './components/Contact'
 import ExperienceGrid from './components/Experience'
-import ProjectsGrid from './components/Projects'
 import Footer from './common_components/Footer'
 
 
@@ -19,8 +18,8 @@ function App() {
   const staticContentList: ContentItemGenerationProps[] = [
     {
       subheader: "About",
-      content: <Text color={"gray.600"} fontSize={"xl"} px={4}>
-        write about section here
+      content: <Text color={"gray.400"} fontSize={"xl"} px={4}>
+        Hey there! I'm Joachim Stanislaus Tan. I'm currently a Frontend Developer @ UBS, working on Pre-trade & Post-trade Transaction Cost Analysis(TCA) Equities & Fx and a FX Orders Management System (Creation & Amendments). I graduated from the University of Exeter with a Bsc Computer Science and a Diploma in Computer Science from Singapore Polytechnic where I acquired skills in machine learning and programming. <br></br><br></br>With a passion for building things, I use my free time to build things I want to see in the world. I am always on the lookout for opportunities to improve myself and my skills. Being able to code we are empowered to breath life into projects that can do so much good for the world, and with great power comes great responsibility. I hope to be able to pursue some passion projects to improve the world in the future!
       </Text>
     },
     {
